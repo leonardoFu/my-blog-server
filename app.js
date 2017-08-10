fbcef8a58eb93964e3b6648010a4a4cd62ff4d7c
leonardoFu <fuyh1@yonyou.com>
2017-08-10 14:41:29 +0800

    提交代码

--- app.js
+++ app.js
@@ -30,14 +30,24 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.set('trust proxy', 1);
 app.use(session({
   secret:'leo',
   resave: false,
   saveUninitialized: true,
-  // cookie: { secure: false }
-}))
+  cookie: {
+    domain: '127.0.0.1',
+    secure: false,
+    path: '/',
+    httpOnly: false
+  }
+}));
+
+
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 //解决跨域问题
 app.all('*', function(req, res, next) {
   res.header('Access-Control-Allow-Credentials', 'true');
